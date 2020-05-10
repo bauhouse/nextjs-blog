8924ea566967ee94f20c53e033825a073b5358f2
Stephen Bau <bauhouse@gmail.com>
2020-05-10 09:11:57 -0700

    Change the site title

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
 const name = 'Stephen Bau'
-export const siteTitle = 'Next.js Sample Website'
+export const siteTitle = 'The Changelog'
 
 export default function Layout({ children, home }) {
   return (
